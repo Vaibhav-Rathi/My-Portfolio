8cd03b9568ea03c4ba4497497f035c212df6b687
Vaibhav-Rathi <vaibhavvaibhav6@gmail.com>
2024-09-28 20:22:41 +0530

    Checking workflows

--- My-Portfolio/src/components/canvas/Ball.tsx
+++ My-Portfolio/src/components/canvas/Ball.tsx
@@ -29,7 +29,6 @@ const Ball: React.FC<BallProps> = (props) => {
           polygonOffsetFactor={-5}
           flatShading
         />
-        
         <Decal
           position={[0, 0, 1]}
           rotation={[2 * Math.PI, 0, 6.25]}
